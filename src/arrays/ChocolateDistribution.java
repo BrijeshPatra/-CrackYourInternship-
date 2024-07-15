9440c563a463c9f6129a78f21c224df315056be1
Brijesh patra <brijeshpatra25@gmail.com>
2024-07-15 11:02:22 +0530

    File added

--- src/arrays/ChocolateDistribution.java
+++ src/arrays/ChocolateDistribution.java
@@ -6,14 +6,16 @@ public class ChocolateDistribution {
         if (arr == null || arr.length == 0 || m <= 0 || m > arr.length) {
             return 0;
         }
+        //sort the array
         Arrays.sort(arr);
 
-        // Find the minimum difference
+        // Find the minimum difference using sliding window approach
         int minDifference = Integer.MAX_VALUE;
         for (int i=0;i+m-1<arr.length;i++){
             int difference = arr[i + m - 1] - arr[i];
             minDifference = Math.min(minDifference, difference);
         }
+        //found the min difference
         return minDifference;
     }
 }
